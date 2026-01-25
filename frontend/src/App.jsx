91e44b97cc5aaaa687cae52b80e785f067e7b130
manampisoa <manampymj@gmail.com>
2026-01-25 19:20:59 +0300

    projet school managment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl  min-h-screen p-2'>
+      <div className='bg-stone-800 rounded-lg h-screen'>
+
+      </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default App
